ee7f51102aada16309698c3cb8be3514c7610c87
Ankit Nimje Account <ankit.nimje93@gmail.com>
2020-06-29 22:23:16 -0400

    Add image upload

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const { stringify } = require('querystring');
 
 const postSchema = mongoose.Schema({
   title: { type: String, required: true },
-  content: { type: String, required: true }
+  content: { type: String, required: true },
+  imagePath: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Post', postSchema);
